8b56c2ee3c2aa85a9ceee099b70ea71a88b376f7
eccs0103 <eccs0103@gmail.com>
2024-02-12 13:37:11 +0400

    1.0.0

---
+++ rules.md
@@ -0,0 +1,5 @@
+# Assembler
+## Initialize
+1. mount D D:\GR210
+## Run
+2. tasm.exe test.asm/zi/l
---
+++ test.asm
@@ -0,0 +1,14 @@
+.model small
+.stack 64
+.data
+a db 'HELLO GR210', 10, 13, '$'
+.code
+start:
+	mov ax, @data
+	mov ds, ax
+	mov ah, 9
+	lea dx, a
+	int 21h
+	mov ax, 4c00h
+	int 21h
+	end start
