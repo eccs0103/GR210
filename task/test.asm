e11995779b24fe387e01b29b4cf591a6c4e0577f
Arman <eccs0103@gmail.com>
2024-05-07 16:59:48 +0400

    2.0.0

--- task/test.asm
+++ task/test.asm
@@ -3,12 +3,33 @@
 .data
 
 .code
-main   proc far
-	mov ax, @data
-	mov ds, ax
-	;#region Main
-	
-	;#endregion
+Main   proc far
+	mov  ax, @data
+	mov  ds, ax
+	;Code #region
+	mov  ax, 64
+	mov  bx, 8
+	call IsDivide
+	mov  ah, 2
+	;mov  dl, dl
+	int  21h
+	;Code #endregion
 	ret
-main endp
-end  main
+Main     endp
+
+; Is `ax` dividing to `bx`
+; Returns (0 | 1) in `dl`
+IsDivide proc
+	push ax bx
+	div  bx
+	cmp  dx, 0
+	jne  False1
+	mov  dl, 1
+	jmp  ProcEnd1
+	False1:
+	mov  dl, 0
+	ProcEnd1:
+	pop  bx ax
+	ret
+IsDivide endp
+end      Main
